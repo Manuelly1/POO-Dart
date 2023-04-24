3b533e6234e6d5ca354fe92befaabfd7b093afdf
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-04-23 22:06:44 -0300

    formulário ok

--- tarefa6/lib/main.dart
+++ tarefa6/lib/main.dart
@@ -5,21 +5,20 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  
-  @override
-  
-  State<StatefulWidget> createState() => _MyAppState();
 
+  @override
+
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final _nomeController = TextEditingController();
   final _emailController = TextEditingController();
-  final _senhaController = TextEditingController();
-  int? _selectedValue = 0; 
+  int? _selectedValue = 0;
+  bool _toggleValue = false;
 
   @override
-  
+
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.deepPurple),
@@ -28,70 +27,92 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: Text("Bora montar a sua rotina?"),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              CustomTextField(
-                controller: _nomeController,
-                hintText: "Digite seu nome:",
-              ),
-              CustomTextField(
-                controller: _emailController,
-                hintText: "Digite seu email:",
-              ),
-              CustomTextField(
-                controller: _senhaController,
-                hintText: "Digite sua senha:",
-              ),
-              CustomRadioButtons(
-                value: 1,
-                groupValue: _selectedValue,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedValue = value;
-                  });
-                },
-                title: "Organizar dias úteis da semana",
-              ),
-              CustomRadioButtons(
-                value: 2,
-                groupValue: _selectedValue,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedValue = value;
-                  });
-                },
-                title: "Organizar o fim de semana",
-              ),
-              CustomElevatedButton(
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text("Processando o cadastro..."),
-                      duration: Duration(seconds: 5),
-                      backgroundColor: Colors.red,
-                    ),
-                  );
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => 
-                        Rotina(_nomeController.text, _emailController.text, _senhaController.text, _selectedValue!,
-
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: <Widget>[
+                CustomTextField(
+                  controller: _nomeController,
+                  hintText: "Digite seu nome:",
+                ),
+                CustomTextField(
+                  controller: _emailController,
+                  hintText: "Digite seu email:",
+                ),
+                SizedBox(height: 16),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Selecione por onde deseja começar:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
                       ),
                     ),
-                  );
-                },
-              ),
-            ],
+                    SizedBox(height: 16),
+                    CustomRadioButtons(
+                      value: 1,
+                      groupValue: _selectedValue,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedValue = value;
+                        });
+                      },
+                      title: "Organizar dias úteis da semana",
+                    ),
+                    CustomRadioButtons(
+                      value: 2,
+                      groupValue: _selectedValue,
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedValue = value;
+                        });
+                      },
+                      title: "Organizar o fim de semana",
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16),
+                CustomToggleButton(
+                  value: _toggleValue,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _toggleValue = value;
+                    });
+                  },
+                  trueText: 'Ainda vou ficar off',
+                  falseText: 'A rotina está on',
+                ),
+                SizedBox(height: 16),
+                CustomElevatedButton(
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text("Processando o cadastro..."),
+                        duration: Duration(seconds: 10),
+                        backgroundColor: Colors.red,
+                      ),
+                    );
+                  },
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: _selectedValue,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text("Atividade ${index + 1}"),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
-
 
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
@@ -100,16 +121,16 @@ class CustomTextField extends StatelessWidget {
   CustomTextField({required this.controller, required this.hintText});
 
   @override
-
+  
   Widget build(BuildContext context) {
-    return TextField(
-      controller: controller,
-      decoration: InputDecoration(hintText: hintText),
-
+    return SingleChildScrollView(
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(hintText: hintText),
+      ),
     );
   }
 }
-
 
 class CustomRadioButtons extends StatelessWidget {
   final int value;
@@ -117,23 +138,19 @@ class CustomRadioButtons extends StatelessWidget {
   final Function(int?)? onChanged;
   final String title;
 
-  CustomRadioButtons({
-    required this.value,
-    required this.groupValue,
-    required this.onChanged,
-    required this.title,
-  });
+  CustomRadioButtons({required this.value, required this.groupValue, required this.onChanged, required this.title});
 
   @override
   
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      leading: Radio(
-        value: value,
-        groupValue: groupValue,
-        onChanged: onChanged,
-  
+    return SingleChildScrollView(
+      child: ListTile(
+        title: Text(title),
+        leading: Radio(
+          value: value,
+          groupValue: groupValue,
+          onChanged: onChanged,
+        ),
       ),
     );
   }
@@ -155,13 +172,60 @@ class CustomElevatedButton extends StatelessWidget {
   }
 }
 
+class CustomToggleButton extends StatefulWidget {
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final String trueText;
+  final String falseText;
+
+  CustomToggleButton({required this.value, required this.onChanged, required this.trueText, required this.falseText});
+
+  @override
+
+  _CustomToggleButtonState createState() => _CustomToggleButtonState();
+
+}
+
+class _CustomToggleButtonState extends State<CustomToggleButton> {
+  late bool _value;
+
+  @override
+  
+  void initState() {
+    super.initState();
+    _value = widget.value;
+  }
+
+  @override
+  
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(widget.falseText),
+        Switch(
+          value: _value,
+          onChanged: (bool value) {
+            setState(() {
+              _value = value;
+            });
+            widget.onChanged(value);
+          },
+        ),
+        Text(widget.trueText),
+      ],
+    );
+  }
+}
+
+
 class Rotina extends StatelessWidget {
   final String nome;
   final String email;
-  final String senha;
   final int selectedValue;
+  final bool toggleValue;
 
-  Rotina(this.nome, this.email, this.senha, this.selectedValue);
+  Rotina(this.nome, this.email, this.selectedValue, this.toggleValue);
 
   @override
   
@@ -170,13 +234,27 @@ class Rotina extends StatelessWidget {
       appBar: AppBar(
         title: Text("Tentando ter uma rotina de gente grande"),
       ),
-      body: ListView(
-        children: List.generate(
-          selectedValue,
-          (index) => ListTile(
-            title: Text("Atividade ${index + 1}"),
-  
-          ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: selectedValue,
+              itemBuilder: (context, index) => ListTile(
+                title: Text("Atividade ${index + 1}"),
+              ),
+            ),
+            CustomToggleButton(
+              value: toggleValue,
+              onChanged: (value) {},
+              trueText: "Completo",
+              falseText: "Incompleto",
+            ),
+            CustomElevatedButton(
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
