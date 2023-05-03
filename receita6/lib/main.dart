16b29c1f1bb9674876c664d24cc216fbe41535de
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-05-02 21:47:39 -0300

    finalização

--- receita6/lib/main.dart
+++ receita6/lib/main.dart
@@ -201,7 +201,7 @@ class DataTableWidget extends StatelessWidget {
   final List<String> columnNames;
   final List<String> propertyNames;
 
-  DataTableWidget({this.jsonObjects = const [], this.columnNames = const ["Nome", "Estilo", "IBU"], this.propertyNames = const ["name", "style", "ibu"]});
+  DataTableWidget({this.jsonObjects = const [], this.columnNames = const [], this.propertyNames = const []});
 
   @override
   
@@ -212,18 +212,16 @@ class DataTableWidget extends StatelessWidget {
               (name) => DataColumn(
                 label: Expanded(child: Text(name, style: TextStyle(fontStyle: FontStyle.italic))),
               ),
-            )
-            .toList(),
+            ).toList(),
         rows: jsonObjects
             .map(
               (obj) => DataRow(
                 cells: propertyNames
                     .map(
                       (propName) => DataCell(Text(obj[propName] ?? "")),
-                    )
-                    .toList(),
+                    ).toList(),
               ),
-            )
-            .toList());
+            ).toList()
+          );
   }
 }
