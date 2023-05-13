acf72aba14a8d37ca45b4205a1f89229a37e91ce
Manuelly Rodrigues Victor <107930253+Manuelly1@users.noreply.github.com>
2023-05-13 13:39:41 -0300

    Q1 ok

--- receita7/lib/main.dart
+++ receita7/lib/main.dart
@@ -3,95 +3,64 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'dart:convert';
 
-class DataService{
-
-  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
+class DataService {
+  final ValueNotifier<List<Map<String, dynamic>>> tableStateNotifier =
+      ValueNotifier([]);
 
   void carregar(int index) async {
-    var res;
-    print('carregar #1 - antes de carregar');
-    if (index == 1) {
-      await carregarCervejas();
-      print('carregar #2 - carregarCervejas concluído');
-      return;
-    }
-    if (index == 2) {
+    if (index == 0) {
       await carregarCafes();
-      print('carregar #3 - carregarCafes concluído');
-      return;
-    }
-    if (index == 3) {
+    } else if (index == 1) {
+      await carregarCervejas();
+    } else if (index == 2) {
       await carregarNacoes();
-      print('carregar #4 - carregarNacoes concluído');
-      return;
     }
   }
-//ainda não está exibindo a parte de cafés e nações
 
-  Future<void> carregarCervejas() async{
-
+  Future<void> carregarCervejas() async {
     var beersUri = Uri(
       scheme: 'https',
       host: 'random-data-api.com',
       path: 'api/beer/random_beer',
-      queryParameters: {'size': '5'});
-
-    print('carregarCervejas #1 - antes do await');
+      queryParameters: {'size': '5'},
+    );
 
     var jsonString = await http.read(beersUri);
-
-    print('carregarCervejas #2 - depois do await');
-
     var beersJson = jsonDecode(jsonString);
 
-    tableStateNotifier.value = beersJson;
-
+    tableStateNotifier.value = List.from(beersJson); 
   }
 
-  Future<void> carregarCafes() async{
-
+  Future<void> carregarCafes() async {
     var coffeeUri = Uri(
       scheme: 'https',
       host: 'random-data-api.com',
       path: 'api/coffee/random_coffee',
-      queryParameters: {'size': '5'});
-
-    print('carregarCafes #1 - antes do await');
+      queryParameters: {'size': '5'},
+    );
 
     var jsonString = await http.read(coffeeUri);
-
-    print('carregarCafes #3 - depois do await');
-
     var coffeesJson = jsonDecode(jsonString);
 
-    tableStateNotifier.value = coffeesJson;
-
+    tableStateNotifier.value = List.from(coffeesJson); 
   }
 
-  Future<void> carregarNacoes() async{
-
+  Future<void> carregarNacoes() async {
     var nationsUri = Uri(
       scheme: 'https',
       host: 'random-data-api.com',
       path: 'api/nation/random_nation',
-      queryParameters: {'size': '5'});
-
-    print('carregarNacoes #1 - antes do await');
+      queryParameters: {'size': '5'},
+    );
 
     var jsonString = await http.read(nationsUri);
-
-    print('carregarNacoes #4 - depois do await');
-
     var nationsJson = jsonDecode(jsonString);
 
-    tableStateNotifier.value = nationsJson;
-
+    tableStateNotifier.value = List.from(nationsJson); 
   }
-
 }
 
 final dataService = DataService();
-
 
 void main() {
 
@@ -116,22 +85,20 @@ class _MyAppState extends State<MyApp> {
     {
       'label': 'Cafés',
       'icon': Icon(Icons.coffee_outlined),
-      'columnNames': ['Nome', 'Tipo', 'Intensidade'],
-      'propertyNames': ['name', 'type', 'strength'],
+      'columnNames': ['Nome', 'Origem', 'Variedade', 'Notas', 'Intensidade'],
+      'propertyNames': ['blend_name', 'origin', 'variety', 'notes', 'intensifier'],
     },
-    
     {
       'label': 'Cervejas',
       'icon': Icon(Icons.local_drink_outlined),
       'columnNames': ['Nome', 'Estilo', 'IBU'],
       'propertyNames': ['name', 'style', 'ibu'],
     },
-    
     {
       'label': 'Nações',
       'icon': Icon(Icons.flag_outlined),
-      'columnNames': ['Nome', 'Continente', 'População'],
-      'propertyNames': ['name', 'continent', 'population'],
+      'columnNames': ['Nacionalidade', 'Idioma', 'Capital', 'Esporte Nac'],
+      'propertyNames': ['nationality', 'language', 'capital', 'national_sport'],
     },
   ];
 
@@ -153,7 +120,6 @@ class _MyAppState extends State<MyApp> {
   
   Widget build(BuildContext context) {
     final navBarItem = _navBarItems[_selectedIndex];
-
     return MaterialApp(
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
@@ -163,7 +129,7 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Dicas'),
         ),
-        body: ValueListenableBuilder<List>(
+        body: ValueListenableBuilder<List<Map<String, dynamic>>>(
           valueListenable: dataService.tableStateNotifier,
           builder: (context, value, child) {
             return DataTableWidget(
@@ -188,6 +154,7 @@ class _MyAppState extends State<MyApp> {
     );
   }
 }
+
 
 
 class NewNavBar extends HookWidget {
@@ -229,7 +196,6 @@ class DataTableWidget extends StatelessWidget {
     final List<String> propertyNames;
 
     DataTableWidget({this.jsonObjects = const [], this.columnNames = const [], this.propertyNames = const []});
-
 
     @override
     
